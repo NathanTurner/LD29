e925582374ea786ddce0f6f4e79327b429a10401
Michael Delfino <michael.a.delfino@gmail.com>
2014-04-26 17:04:43 -0400

    Add ocean and tweak shark movement
    And the earth was without form, and void; and darkness was upon the face
    of the deep. And the Spirit of God moved upon the face of the waters
    (Genesis 1:2).

--- js/game.js
+++ js/game.js
@@ -1,12 +1,14 @@
 // vim: expandtab:sw=4:ts=4:sts=4
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
 var SPRITE_DIR = 'assets/sprites/';
+var SHARK_SPEED = 10;
 
 function preload() {
     game.load.image('shark', SPRITE_DIR + 'shark.png');
     game.load.image('jet',   SPRITE_DIR + 'jet.svg');
+    game.load.image('water', SPRITE_DIR + 'bg.png');
 }
 
 var player;
@@ -15,13 +17,20 @@ var jets;
 function create() {
     game.stage.backgroundColor = '#202040';
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.world.setBounds(0, 0, 800, 600);
+    game.world.setBounds(0, 0, 928, 600);
     cursors = game.input.keyboard.createCursorKeys();
+    cursors.w = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    cursors.a = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    cursors.d = game.input.keyboard.addKey(Phaser.Keyboard.D);
 
-    player = game.add.sprite(game.world.centerX / 2, game.world.centerY * 3/2, 'shark');
+    ocean = game.add.tileSprite(0,300,800,300, 'water');
+
+    player = game.add.sprite(game.world.centerX, game.world.centerY * 3/2, 'shark');
     player.anchor.setTo(0.5, 0.5);
-    player.scale.setTo(0.5,0.5);
+    player.scale.setTo(0.3, 0.3);
+
     game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.maxVelocity.setTo(300, 1000);
 
     jets = game.add.group();
     jets.enableBody = true;
@@ -43,23 +52,57 @@ function createJet() {
 
 function update() {
     game.physics.arcade.overlap(player, jets, collisionHandler, null, this);
-
-    //  Reset the player, then check for movement keys
-    player.body.velocity.setTo(0, 0);
     player.body.angularVelocity = 0;
+    if (player.body.y > 300) {
+        
+        if (cursors.up.isDown || cursors.w.isDown)
+        {
+            player.body.velocity.x += game.physics.arcade.velocityFromAngle(player.angle, SHARK_SPEED).x;
+            player.body.velocity.y += game.physics.arcade.velocityFromAngle(player.angle, SHARK_SPEED).y;
+        }
+        else if (player.body.velocity.x > 0)
+        {
+            player.body.velocity.x -= 10;
+        }
 
-    if (cursors.up.isDown)
-    {
-        player.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(player.angle, 300));
+        player.body.velocity.y += player.angle/10;
+
+        if ((cursors.left.isDown || cursors.a.isDown) && player.angle > -45)
+        {
+            player.body.angularVelocity = -200;
+        }
+        else if ((cursors.right.isDown || cursors.d.isDown) && player.angle < 45)
+        {
+            player.body.angularVelocity = 200;
+        }
+        if (!cursors.right.isDown && !cursors.d.isDown && !cursors.left.isDown && !cursors.a.isDown) {
+            player.angle += 1 * (player.angle < 0)
+            player.angle += -1 * (player.angle > 0)
+        }
+        if (player.body.y + player.height > game.height)
+        {
+            player.body.velocity.y = 0;
+            player.body.y = game.height - player.height;
+        }
+
+        player.body.velocity.x -= SHARK_SPEED * ((player.body.x + player.width) / 600);
     }
-    if (cursors.left.isDown)
+    else
     {
-        player.body.angularVelocity = -300;
+        player.angle += 1 * (player.angle - 75 < 0)
+        player.angle += -1 * (player.angle - 75 > 0)
+        player.body.velocity.x -= 1;
+        player.body.velocity.y += 10;
     }
-    else if (cursors.right.isDown)
+    if (player.body.x < -player.width/2)
     {
-        player.body.angularVelocity = 300;
+        player.body.velocity.x = 0;
+        player.body.x = -player.width/2;
     }
+    ocean.tilePosition.x -= 5;
+}
+
+function render() {
 }
 
 function collisionHandler (shark, jet)
