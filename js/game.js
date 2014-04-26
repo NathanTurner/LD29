ae8b48d52182e67aa220f2fdd57056613d5c47c8
Michael Delfino <michael.a.delfino@gmail.com>
2014-04-26 12:07:14 -0400

    Change shark movement
    And all that have not fins and scales in the seas, and in the rivers, of all that move in the waters, and of any living thing which is in the waters, they shall be an abomination unto you (Leviticus 11:10)

--- js/game.js
+++ js/game.js
@@ -8,31 +8,30 @@ var player;
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.world.setBounds(0, 0, 800, 600);
+    cursors = game.input.keyboard.createCursorKeys();
+
     player = game.add.sprite(game.world.centerX / 2, game.world.centerY * 3/2, 'shark');
     player.anchor.setTo(0.5, 0.5);
     game.physics.enable(player, Phaser.Physics.ARCADE);
-    cursors = game.input.keyboard.createCursorKeys();
+    
 }
 
 function update() {
     //  Reset the player, then check for movement keys
     player.body.velocity.setTo(0, 0);
-
-    if (cursors.left.isDown)
-    {
-        player.body.velocity.x = -200;
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.velocity.x = 200;
-    }
+    player.body.angularVelocity = 0;
 
     if (cursors.up.isDown)
     {
-        player.body.velocity.y = -200;
+        player.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(player.angle, 300));
     }
-    else if (cursors.down.isDown)
+    if (cursors.left.isDown)
     {
-        player.body.velocity.y = 200;
+        player.body.angularVelocity = -300;
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.angularVelocity = 300;
     }
 }
