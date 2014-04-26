6e8492962997eaf01a85c22bfd966ebc03b0e97b
Nathan Turner <NathanTurnerDev@gmail.com>
2014-04-26 14:28:08 -0400

    Add a jet on screen

--- js/game.js
+++ js/game.js
@@ -1,7 +1,12 @@
+// vim: expandtab:sw=4:ts=4:sts=4
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+var SPRITE_DIR = 'assets/sprites/';
+
 function preload() {
-    game.load.image('shark', 'assets/sprites/shark.png');
+    game.load.image('shark', SPRITE_DIR + 'shark.png');
+    game.load.image('jet',   SPRITE_DIR + 'jet.svg');
 }
 
 var player;
@@ -14,7 +19,16 @@ function create() {
     player = game.add.sprite(game.world.centerX / 2, game.world.centerY * 3/2, 'shark');
     player.anchor.setTo(0.5, 0.5);
     game.physics.enable(player, Phaser.Physics.ARCADE);
-    
+
+    createJet();
+}
+
+function createJet() {
+    var jet = game.add.sprite(0, 0, 'jet');
+    var cropRect = {x: 0, y: 0, width: jet.width, height: 35};
+    jet.crop(cropRect);
+    var jetTween = game.add.tween(jet);
+    jetTween.to({ x: game.width + (1600 + jet.x) }, 10000, Phaser.Easing.Linear.None, true);
 }
 
 function update() {
